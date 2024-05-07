b64ae320c19da3173a3d92ba27d4779cbc4d0037
zahrabateninia <zahrabatenin@gmail.com>
2024-05-07 19:57:28 +0330

    Initialize a queue data structure with the starting square as the first element

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // knightMoves() takes two square coordinates as input 
 function knightMoves(start, end){
     // possible moves for a knight
+    // each move is represented by two elements: the change in x-coordinate and the change in y-coordinate
     const moves = [
         [1, 2],
         [2, 1],
@@ -12,5 +13,12 @@ function knightMoves(start, end){
         [-2, -1],
         [-2, 1],
         [-1, 2]
-    ]
+    ];
+
+    let chessBoardSize = 8;
+    // define a queue to perform BFS 
+    const queue = [[start]]; 
+
+    
+
 }
