44a76861959160f67a1e1824408847e91d7de4a8
zahrabateninia <zahrabatenin@gmail.com>
2024-05-08 11:13:08 +0330

    Mark the start square as visited(true) and create path variable

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function knightMoves(start, end){
     const queue = [[start]]; 
 
     // create a 2D array
-    const visited = Array.from({ length: boardSize }, () => Array(boardSize).fill(false));
+    const visited = Array.from({ length: boardSize }, () => Array(boardSize).fill(false)); //?????????????????/
     // output of visited array for board size of 8 is : 
     // [
     //     [false, false, false, false, false, false, false, false],
@@ -35,6 +35,16 @@ function knightMoves(start, end){
     // By initializing the visited array with false, we are essentially saying that no squares have been visited initially.
     // Then, as we explore squares during the BFS process, we update the visited array accordingly to mark the squares that have been visited. 
     // This helps us avoid revisiting them and ensures that we find the shortest path efficiently.
+
+    // mark the start square as visited 
+    visited[start[0]][start[1]] = true;
+
+    // iterate until the queue is empty
+    while(queue.length > 0){
+        const path = queue.shift();
+        // get the last square of the path using destructuring assignment
+        const [x, y] = path[path.length - 1] 
+    }
 
 
 }
